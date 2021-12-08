24ac1042f0f1e4e8d840c85e0e6c0df69c357185
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2021-12-08 09:19:09 +0530

    color schema

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/Header.module.css'
     return (
         <div data-aos="fade-up" className={styles.container}>
         <div className={styles.h1}>
-            <h1>Hey👋Myself SARTHAK BINDAL</h1><br/>
+            <h1>Hey&#128075;Myself SARTHAK BINDAL</h1><br/>
             <p>I am a sophomore at IIIT KOTTAYAM, pursuing BTech in Computer Science Engineering with an interest in Competitive Coding. I have done some frontend work in web and app development.
 Looking forward to connect with people from diverse backgrounds.</p>
         </div>
